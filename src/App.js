085ba0c25e6a5834709b101c544db4cbe688afd9
slay_trunsks <slaytrunsks@gmail.com>
2022-10-22 10:33:51 +0545

    pages and router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from './pages/Home'
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Error from './pages/Error'
 function App() {
   return (
-    <>
-    <h1>hello world!</h1>
-    </>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    <Route path="/forgot-password" element={<ForgotPassword />} />
+    <Route path="/offers" element={<Offers />} />
+    <Route path="*" element={<Error />} />
+   </Routes>
+   </BrowserRouter>
   );
 }
 
